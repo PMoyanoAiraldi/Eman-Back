2ac92f2d61af8a81626e56245df98f60bdf941b2
PMoyanoAiraldi <pmoyano497@gmail.com>
2026-04-15 18:43:56 -0300

    Creacion de funcion createCategories

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Categories } from './categories.entity';
+import { CreateCategoriesDto } from './dto/create-categories.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -9,4 +10,25 @@ export class CategoriesService {
     @InjectRepository(Categories)
         private readonly categoriesRepository: Repository<Categories>,
     ) { }
+
+    async createCategories(createCategoriesDto: CreateCategoriesDto): Promise<Categories>{
+            const normalizedName = createCategoriesDto.name.trim().toLowerCase();
+        
+            const existingCategories = await this.categoriesRepository
+                .createQueryBuilder('categories')
+                .where('LOWER(brand.name) = :name', { name: normalizedName })
+                .getOne();
+        
+            if (existingCategories) {
+                throw new HttpException(`La categoria "${createCategoriesDto.name}" ya existe.`, HttpStatus.BAD_REQUEST);
+            }   
+            
+            const categories = this.categoriesRepository.create({
+                name: createCategoriesDto.name.trim()
+            });
+            console.log("Categoria antes de ser guardada", categories)
+    
+            return await this.categoriesRepository.save(categories);
+        }
+
 }
---
+++ src/categories/dto/create-categories.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+export class CreateCategoriesDto {
+    
+    @ApiProperty({ description: "El nombre de la categoria", required: true})
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
+    name: string;
+
+}
